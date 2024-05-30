d250d692658b758bd061f5203614dfa8deff3ba2
chen <alf138540fun@gmail.com>
2024-05-31 01:09:37 +0200

    Add search hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Card from './Components/Card/Card';
 import CardList from './Components/CardList/CardList';
+import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
