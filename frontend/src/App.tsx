888bdfed94eb5626fe2e8a36f1f5d7860ba7b0c9
chen <alf138540fun@gmail.com>
2024-05-31 17:54:37 +0200

    Add type narrowing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,25 @@ import './App.css';
 import Card from './Components/Card/Card';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
   }
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
   };
   return (
     <div className="App">
