d7a59732a4ee28f0328433fec329cd29acfc4ff2
chen <alf138540fun@gmail.com>
2024-05-31 18:15:21 +0200

    Add conditional rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,6 +26,7 @@ function App() {
   return (
     <div className="App">
       <Search onClick={onClick} search={search} handleChange={handleChange} />
+      {serverError && <h1>{serverError}</h1>}
       <CardList />
     </div>
   );
